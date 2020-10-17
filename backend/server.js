d7ba7d4335670c1b71c2c3c6f5875aba0eaad6ac
manish-verma-au3 <rohitverma@gmail.com>
2020-10-17 23:44:16 +0530

    pp

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const products = require('./data/products');
+const dotenv = require('dotenv');
+
+dotenv.config()
+
+const app = express();
+
+app.get('/', (req, res) =>{
+    res.send('API is running..')
+});
+
+app.get('/api/products', (req, res) =>{
+    res.send(products);
+});
+
+app.get('/api/products/:id', (req, res) =>{
+    const product = products.find((p) => p._id === req.params.id)
+    res.send(product);
+});
+
+const PORT = process.env.PORT || 5000
+
+app.listen(5000, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
